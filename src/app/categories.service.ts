ffbfe2d5fdda5f11d2ac1b64f990619e92330650
Andrzej Fricze <fricze@MacBook-Pro-Andrzej.local>
2019-09-26 23:37:37 +0200

    Categories

--- src/app/categories.service.ts
+++ src/app/categories.service.ts
@@ -11,17 +11,17 @@ type Category = {
 
 const newCategoryFromName = (name: string) => ({ name, id: name, new: true })
 
+const initialCategories = [
+    { name: "Private", id: "46004df1-876a-443c-9126-4bee714bed9e", },
+    { name: "Public", id: "4cc5e97c-7572-481a-969d-e92b131a2e8d", },
+]
+
 @Injectable({
     providedIn: 'root'
 })
 export class CategoriesService {
-    private initialCategories = [
-        { name: "Private", id: "priv", },
-        { name: "Public", id: "pub", },
-    ]
-
     newCategory$: Subject<string> = new Subject()
-    activeCategory$ = new BehaviorSubject(this.initialCategories[0].id)
+    activeCategory$ = new BehaviorSubject(initialCategories[0].id)
 
     private newCategories$ = this.newCategory$.pipe(
         scan((categories, name: string) =>
@@ -29,7 +29,9 @@ export class CategoriesService {
         startWith([]),
     )
 
-    categories$ = combineLatest(of(this.initialCategories), this.newCategories$).pipe(
+    categories$ = combineLatest(
+        of(initialCategories), this.newCategories$
+    ).pipe(
         map((collection: Array<Array<Category>>) => flatten(collection)),
     )
 }
